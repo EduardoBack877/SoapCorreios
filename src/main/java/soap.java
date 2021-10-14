ee97ad0733b41b4d1393f382b370a8238f5bb408
Eduardo <Eduardoback87@gmail.com>
2021-10-13 23:15:21 -0300

    fdsf

---
+++ src/main/java/soap.java
@@ -0,0 +1,25 @@
+
+import java.util.List;
+import org.tempuri.CResultado;
+import org.tempuri.CServico;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dudub
+ */
+public class soap {
+    
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Tela().setVisible(true);
+            }
+        });
+    }
+}
